33d69519db8e5dbfdb3be25ef7a7ed52f84b69db
Juan Pardo <josefrecinosanitarios@yahoo.com.ar>
2024-09-17 11:20:49 -0300

    q

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className='container'>
         <p>Intoducing Blockforge</p>
         <h1>The future of blockchain is here.</h1>
-        <p>Lorem ipsum </p>
+        <p>Lorem ipsumm </p>
         <CutCornerButton title='Get started' />
       </div>
     </section>
